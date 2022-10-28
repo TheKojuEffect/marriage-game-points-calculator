59ff9fb91cea49324ee9c9c18a0dc0ff8560d5d6
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-27 23:37:07 -0400

    Basic settings page

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,13 +1,84 @@
-import {Button, Card, CardContent, CardHeader, Stack} from "@mui/material";
+import {Box, Button, Divider, Stack, TextField} from "@mui/material";
 import {FC} from "react";
+import {useForm} from "react-hook-form";
+import {useRouter} from "next/router";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import {startCase} from "lodash";
+
+type GameSettings = {
+    pointRate: number;
+    seenPoint: number;
+    unseenPoint: number;
+    foulPoint: number;
+    dubleeWinBonusPoint: number;
+}
+
+const defaultGameSettings: GameSettings = {
+    pointRate: 1,
+    seenPoint: 3,
+    unseenPoint: 10,
+    dubleeWinBonusPoint: 5,
+    foulPoint: 15,
+}
 
 export const Settings: FC = () => {
-    return (
-        <Card>
-            <CardHeader title="Settings"/>
-            <CardContent>
+    const router = useRouter();
+    const {register, handleSubmit, control, formState: {errors}} = useForm<GameSettings>({
+        defaultValues: defaultGameSettings
+    });
 
-            </CardContent>
-        </Card>
-    )
+    const onSubmit = (data: GameSettings) => {
+        console.log({data})
+    };
+
+    return (
+        <Box>
+            <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)}>
+                <List
+                    subheader={<ListSubheader color="primary">New Game Settings</ListSubheader>}
+                >
+                    {
+                        (Object.keys(defaultGameSettings) as (keyof GameSettings)[])
+                            .map(setting =>
+                                <ListItem key={setting}>
+                                    <ListItemText primary={startCase(setting)}/>
+                                    <TextField
+                                        inputProps={{style: {textAlign: "center"}}}
+                                        sx={{width: '7ch', textAlign: 'center'}}
+                                        variant="outlined"
+                                        required
+                                        size="small"
+                                        {...register(setting, {required: true, min: 0})}
+                                        error={!!errors[setting]}
+                                    />
+                                </ListItem>
+                            )
+                    }
+                </List>
+
+                <Stack direction="row" spacing={2}>
+                    <Button
+                        type="reset"
+                        fullWidth
+                        variant="outlined"
+                        color="primary"
+                        onClick={router.back}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                    >
+                        Start
+                    </Button>
+                </Stack>
+            </Box>
+        </Box>
+    );
 };
