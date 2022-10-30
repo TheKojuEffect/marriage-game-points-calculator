85f84d3d880e8bfb2f3557b805a58d126933b5a6
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-30 17:48:57 -0400

    Add link to homepage

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import {Container} from "@mui/material";
+import Link from "next/link";
 
 export const NavBar: FC = () => (
     <Box sx={{flexGrow: 1}}>
@@ -21,16 +22,19 @@ export const NavBar: FC = () => (
                     >
                         <MenuIcon/>
                     </IconButton>
-                    <Typography variant="h6"
-                                component="div"
-                                sx={{
-                                    flexGrow: 1,
-                                    cursor: "pointer",
-                                    caretColor: "transparent"
-                                }}
-                    >
-                        Marriage
-                    </Typography>
+                    <Link href="/" legacyBehavior>
+                        <Typography variant="h6"
+                                    component="a"
+                                    sx={{
+                                        flexGrow: 1,
+                                        cursor: "pointer",
+                                        caretColor: "transparent",
+                                        textDecoration: "none",
+                                    }}
+                        >
+                            Marriage
+                        </Typography>
+                    </Link>
                 </Toolbar>
             </Container>
         </AppBar>
