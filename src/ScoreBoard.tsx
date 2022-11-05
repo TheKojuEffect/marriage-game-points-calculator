4da2b384f11c5574f9361b60e63139fb36c7f72a
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-11-05 13:45:31 -0400

    Show scores

--- src/ScoreBoard.tsx
+++ src/ScoreBoard.tsx
@@ -4,13 +4,14 @@ import {usePlayers} from "./usePlayers";
 import {useScores} from "./useScores";
 import {useRounds} from "./useRounds";
 import {Loading} from "./Loading";
-
-type ScoreLine = {
-    roundId: string;
-    roundDate: Date;
-    playerId: string;
-    point: number;
-}
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {DbPlayer, DbRound} from "./db";
 
 export const ScoreBoard: FC<GameIdProp> = ({gameId}) => {
     const rounds = useRounds(gameId);
@@ -21,7 +22,40 @@ export const ScoreBoard: FC<GameIdProp> = ({gameId}) => {
         return <Loading/>;
     }
 
-    console.log({rounds, scores, players});
+    const getPoint = ({id: roundId}: DbRound, {id: playerId}: DbPlayer): number =>
+        scores.find(s => s.playerId === playerId && s.roundId === roundId)?.point ?? 0;
 
-    return <span>Score Board {gameId}</span>;
-};
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Round</TableCell>
+                        {
+                            players.map(player =>
+                                <TableCell key={player.id} align="center">{player.name}</TableCell>
+                            )
+                        }
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        rounds.map(round =>
+                            <TableRow key={round.id}>
+                                <TableCell>
+                                    {round.createdAt.toLocaleString()}
+                                </TableCell>
+                                {
+                                    players.map(player =>
+                                        <TableCell align="center">{getPoint(round, player)}</TableCell>
+                                    )
+                                }
+
+                            </TableRow>
+                        )
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
